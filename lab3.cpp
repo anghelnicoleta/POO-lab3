38d2e091f3a63d0793cae23069580bd566eae60c
anghelnicoleta <80319360+anghelnicoleta@users.noreply.github.com>
2021-05-10 12:45:37 +0300

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class tablou
+{
+public:
+    string denumire;
+    string curent;
+    string autor;
+    tablou(string x, string y, string z);
+};
+tablou::tablou(string x, string y, string z)
+{
+    denumire = x;
+    curent = y;
+    autor = z;
+}
+int main()
+{
+    tablou tablouObj1("Al 9-lea Val", "Romantism", "Ivan Aivazovsky");
+    cout << tablouObj1.denumire << " " << tablouObj1.curent << " " << tablouObj1.autor << " " << endl;
+    tablou tablouObj2("Edecarii de pe Volga", "Realism", "Ilya Repin");
+    cout << tablouObj2.denumire << " " << tablouObj2.curent << " " << tablouObj2.autor << " " << endl;
+    tablou tablouObj3("Fata cu piersici", "Ipresionism", "Valentin Serov");
+    cout << tablouObj3.denumire << " " << tablouObj3.curent << " " << tablouObj3.autor << " " << endl;
+    return 0;
+}
